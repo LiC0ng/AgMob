502495c8f40817e5d98e5c5936550aeb049410d2
linsho <kaku.l.aa@rio.gsic.titech.ac.jp>
2019-06-21 18:19:42 +0900

    add feature post to localhost:8080/api/session

--- driver/src/renderer.ts
+++ driver/src/renderer.ts
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const driverStartButton = document.querySelector("#driver_start_button");
+
+driverStartButton.addEventListener("click", function(clickEvent) {
+   const xhr = new XMLHttpRequest();
+   xhr.open("POST", "http://localhost:8080/api/session");
+   xhr.send();
+   xhr.onreadystatechange = function(){
+       if(xhr.readyState === 4 && xhr.status === 200) {
+           console.log(xhr.responseText);
+       }
+   }
+})
+
