8d3fc24791fa97d0d72952c8c6c2d5a19e840698
linsho <kaku.l.aa@rio.gsic.titech.ac.jp>
2019-06-28 14:39:33 +0900

    ドライバー: ワークスペースとのWebSocketを作成する

--- driver/src/renderer.ts
+++ driver/src/renderer.ts
@@ -2,16 +2,50 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+import {log} from "util";
+
 const driverStartButton = document.querySelector("#driver_start_button");
+const websocketStartButton = document.querySelector("#websocket_create_button");
+const websocketCloseButton = document.querySelector("#websocket_close_button");
 
-driverStartButton.addEventListener("click", function(clickEvent) {
-   const xhr = new XMLHttpRequest();
-   xhr.open("POST", "http://localhost:8080/api/session");
-   xhr.send();
-   xhr.onreadystatechange = function(){
-       if(xhr.readyState === 4 && xhr.status === 200) {
-           console.log(xhr.responseText);
-       }
-   }
-})
+let uuid;
 
+
+driverStartButton.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/api/session");
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            uuid = xhr.responseText;
+            // tslint:disable-next-line:no-console
+            console.log(uuid);
+        }
+    };
+});
+
+let connection: WebSocket;
+const socketUrl = "wss://echo.websocket.org";
+
+websocketStartButton.addEventListener("click", () => {
+    console.log("!");
+    connection = new WebSocket(socketUrl);
+    connection.onopen = (e) => {
+        connection.send("hoge");
+    };
+    connection.onmessage = (e) => {
+        // tslint:disable-next-line:no-console
+        console.log(e.data);
+    };
+    connection.onerror = (e) => {
+        console.log(e);
+    };
+    connection.onclose = (e) => {
+        console.log(e);
+    };
+
+});
+
+websocketCloseButton.addEventListener("click", () => {
+    connection.close();
+});
