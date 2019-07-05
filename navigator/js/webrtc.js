983f020d4a3bbac30ba4724976a8fe2c4ac971e2
LiC0ng <a1040484@gmail.com>
2019-07-05 15:03:00 +0900

    add start communication

---
+++ navigator/js/webrtc.js
@@ -0,0 +1,16 @@
+// WebRTCを利用する準備をする
+function prepareNewConnection(isOffer) {
+    const pc_config = {"iceServers":[ {"urls":"stun:stun.webrtc.ecl.ntt.com:3478"} ]};
+    const peer = new RTCPeerConnection(pc_config);
+
+    return peer;
+}
+
+// 手動シグナリングのための処理を追加する
+function sendSdp(sessionDescription) {
+    console.log('---sending sdp ---');
+    textForSendSdp.value = sessionDescription.sdp;
+    textForSendSdp.focus();
+    textForSendSdp.select();
+}
+
