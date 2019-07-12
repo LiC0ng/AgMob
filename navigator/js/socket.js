a79b5511c72c987cfb8399c9996eac8c0318e088
Yunato <yunato525@gmail.com>
2019-07-12 15:22:24 +0900

    Navigator: fix format about url to follow driver's code

--- navigator/js/socket.js
+++ navigator/js/socket.js
@@ -1,12 +1,12 @@
 let peer;
-const domain = "160.16.213.209";
+const WORKSPACE_BASE_ADDRESS = "160.16.213.209";
 const pcConfig = {iceServers: [{urls: "stun:stun.l.google.com:19302"}]};
 let video = document.getElementById("agmob-screen-viewer");
 
 
 function sendWebsocket() {
     var id = document.getElementById("session-id").value;
-    var url = "ws://" + domain + ":8080/api/session/" + id + "/navigator";
+    var url = `ws://${WORKSPACE_BASE_ADDRESS}:8080/api/session/${id}/navigator`;
     var ws = new WebSocket(url);
 
     ws.onopen = function() {
