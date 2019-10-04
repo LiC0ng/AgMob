1db464a0cc247fc69571c5168db97043d7f00d51
Kazuki Yamaguchi <k@rhe.jp>
2019-10-04 16:24:28 +0900

    navigator: remove wait-new-driver.tsx as it is merged into NavigatorApp

--- navigator/src/index.tsx
+++ navigator/src/index.tsx
@@ -7,14 +7,12 @@ import "./index.css";
 
 import HomeApp from "./HomeApp";
 import NavigatorApp from "./NavigatorApp";
-import WaitNewDriver from "./wait-new-driver";
 
 const App = () => (
   <div>
     <Switch>
       <Route exact={true} path="/"><HomeApp /></Route>
       <Route path="/session/" component={ NavigatorApp } />
-      <Route path="/wait" component={ WaitNewDriver } />
     </Switch>
   </div>
 );
