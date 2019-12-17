fcb4062bb3946a122996562bd6540689203150ec
Kazuki Yamaguchi <k@rhe.jp>
2019-12-17 18:57:10 +0900

    driver, navigator: update theme color

--- navigator/src/index.tsx
+++ navigator/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap-override.scss";
 import "bootstrap";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
