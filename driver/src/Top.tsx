66ce5ece18e9703250bb79271e6c1e2ba7b2f02d
Kazuki Yamaguchi <k@rhe.jp>
2019-12-17 14:26:59 +0900

    driver: remove Debug button from the top page
    It is not useful anymore, as we can stop a driver session on our own.

--- driver/src/Top.tsx
+++ driver/src/Top.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import {PropsWithSession} from "./types";
 
 interface Props extends PropsWithSession { }
@@ -30,12 +30,6 @@ export default class Top extends React.Component<Props, State> {
                     </Col>
                 </Row>
 
-                <Row className={"justify-content-md-end fixed-bottom"} style={{ paddingBottom: 40, paddingRight: 15 }}>
-                    <Col md={2}>
-                        <Link className={"btn btn-outline-primary btn-lg btn-block"} to={"/end"} style={{ fontSize: 15 }}>{"Debug"}</Link>
-                    </Col>
-                </Row>
-
                 <Row className={"justify-content-md-end fixed-bottom"} style={{ paddingRight: 20 }}>
                     <Col md={"auto"}>
                         <p>version {require("../package.json").version}</p>
@@ -45,5 +39,3 @@ export default class Top extends React.Component<Props, State> {
         );
     }
 }
-
-
